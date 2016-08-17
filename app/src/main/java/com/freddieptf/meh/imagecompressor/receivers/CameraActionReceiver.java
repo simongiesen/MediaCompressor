9a85ef0f85f84aa15f76b97d2d0c41b4ec02a066
freddieptf <freddmuiru@gmail.com>
2016-08-17 22:31:33 +0300

    add preference check

--- app/src/main/java/com/freddieptf/meh/imagecompressor/receivers/CameraActionReceiver.java
+++ app/src/main/java/com/freddieptf/meh/imagecompressor/receivers/CameraActionReceiver.java
@@ -6,21 +6,29 @@ import android.content.Intent;
 import android.util.Log;
 
 import com.freddieptf.meh.imagecompressor.services.CameraActionHandlerService;
+import com.freddieptf.meh.imagecompressor.utils.PrefUtils;
 
 /**
  * Created by freddieptf on 16/07/16.
  */
 public class CameraActionReceiver extends BroadcastReceiver {
     private static final String TAG = "CameraActionReceiver";
+
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d(TAG, "OnReceive");
-        if(intent != null && intent.getData() != null) {
+
+        if (intent != null && intent.getData() != null) {
+
             Log.d(TAG, intent.getData() + "\n" + intent.getAction());
-            Intent compressServiceIntent = new Intent(context, CameraActionHandlerService.class);
-            compressServiceIntent.putExtra(CameraActionHandlerService.MEDIA_URI, intent.getData().toString());
-            compressServiceIntent.setAction(intent.getAction());
-            context.startService(compressServiceIntent);
+
+            if (intent.getAction().equals("android.hardware.action.NEW_PICTURE")
+                    && PrefUtils.canGetPictureBroadcast(context)) {
+                Intent compressServiceIntent = new Intent(context, CameraActionHandlerService.class);
+                compressServiceIntent.putExtra(CameraActionHandlerService.MEDIA_URI, intent.getData().toString());
+                compressServiceIntent.setAction(intent.getAction());
+                context.startService(compressServiceIntent);
+            }
         }
     }
 }
