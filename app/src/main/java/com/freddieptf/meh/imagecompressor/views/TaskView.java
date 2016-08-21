aeff0bdd9607644f67a9a1f725fd8f04c038e9d1
freddieptf <freddmuiru@gmail.com>
2016-08-21 17:49:52 +0300

    Use a simple bundle to save state
    saving the state manually using a BaseSavedState was causing a crash on a kitkat test device

--- app/src/main/java/com/freddieptf/meh/imagecompressor/views/TaskView.java
+++ app/src/main/java/com/freddieptf/meh/imagecompressor/views/TaskView.java
@@ -2,14 +2,10 @@ package com.freddieptf.meh.imagecompressor.views;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.os.Parcel;
+import android.os.Bundle;
 import android.os.Parcelable;
-import android.support.v4.os.ParcelableCompat;
-import android.support.v4.os.ParcelableCompatCreatorCallbacks;
 import android.text.TextUtils;
 import android.util.AttributeSet;
-import android.util.Log;
-import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,6 +26,7 @@ public class TaskView extends LinearLayout implements View.OnClickListener {
     private static final String TAG = "TaskView";
     OnTaskCheck onTaskCheck;
     boolean checked = false;
+    private String VIEW_SAVE_STATE = "view_save_state";
 
     public TaskView(Context context) {
         this(context, null);
@@ -71,13 +68,6 @@ public class TaskView extends LinearLayout implements View.OnClickListener {
         taskRadioButton.setChecked(checked);
     }
 
-    public void setChecked(boolean checked) {
-        Log.d(TAG, getTag() + " SetChecked: " + checked);
-        this.checked = checked;
-        taskRadioButton.setChecked(checked);
-        expand(checked);
-    }
-
     private void expand(boolean checked){
         taskBody.setVisibility(checked ? VISIBLE : GONE);
     }
@@ -98,64 +88,17 @@ public class TaskView extends LinearLayout implements View.OnClickListener {
     @Override
     protected Parcelable onSaveInstanceState() {
         Parcelable p = super.onSaveInstanceState();
-        SaveState saveState = new SaveState(p);
-        saveState.childrenStates = new SparseArray<Parcelable>();
-        for(int i = 0; i < getChildCount(); i++){
-            getChildAt(i).saveHierarchyState(saveState.childrenStates);
-        }
-        return saveState;
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(VIEW_SAVE_STATE, p);
+        bundle.putBoolean(getTag() + "_checked", taskRadioButton.isChecked());
+        return bundle;
     }
 
     @Override
     protected void onRestoreInstanceState(Parcelable state) {
-        SaveState saveState = (SaveState) state;
-        super.onRestoreInstanceState(state);
-        for(int i = 0; i < getChildCount(); i++){
-            getChildAt(i).restoreHierarchyState(saveState.childrenStates);
-        }
-        if(taskRadioButton.isChecked()) expand(true);
-    }
-
-    @Override
-    protected void dispatchSaveInstanceState(SparseArray<Parcelable> container) {
-        dispatchFreezeSelfOnly(container);
-    }
-
-    @Override
-    protected void dispatchRestoreInstanceState(SparseArray<Parcelable> container) {
-        dispatchThawSelfOnly(container);
-    }
-
-    static class SaveState extends BaseSavedState {
-        SparseArray childrenStates;
-
-        SaveState(Parcelable source) {
-            super(source);
-        }
-
-        private SaveState(Parcel source, ClassLoader loader) {
-            super(source);
-            childrenStates = source.readSparseArray(loader);
-        }
-
-        @Override
-        public void writeToParcel(Parcel out, int flags) {
-            super.writeToParcel(out, flags);
-            out.writeSparseArray(childrenStates);
-        }
-
-        public static final Creator<SaveState> CREATOR =
-                ParcelableCompat.newCreator(new ParcelableCompatCreatorCallbacks<SaveState>() {
-
-            @Override
-            public SaveState createFromParcel(Parcel source, ClassLoader loader) {
-                return new SaveState(source, loader);
-            }
-            @Override
-            public SaveState[] newArray(int size) {
-                return new SaveState[size];
-            }
-        });
+        Bundle bundle = (Bundle) state;
+        super.onRestoreInstanceState(bundle.getParcelable(VIEW_SAVE_STATE));
+        if(bundle.getBoolean(getTag() + "_checked")) expand(true);
     }
 
 }
